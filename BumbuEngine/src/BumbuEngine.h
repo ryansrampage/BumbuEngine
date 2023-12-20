222413bb97d9c1ad65877eafadf54231d3f8f500
ryansrampage <ryanslee.47@gmail.com>
2023-12-20 19:39:58 +0800

    Added spdlog and wrapper

--- BumbuEngine/src/BumbuEngine.h
+++ BumbuEngine/src/BumbuEngine.h
@@ -3,6 +3,7 @@
 // For use by BumbuEngine Applications
 
 #include "BumbuEngine/Application.h"
+#include "BumbuEngine/Log.h"
 
 // ---Entry Point---------------------------
 #include "BumbuEngine/EntryPoint.h"
--- BumbuEngine/src/BumbuEngine/EntryPoint.h
+++ BumbuEngine/src/BumbuEngine/EntryPoint.h
@@ -6,6 +6,8 @@ extern BumbuEngine::Application* BumbuEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	BumbuEngine::Log::Init();
+
 	auto app = BumbuEngine::CreateApplication();
 	app->Run();
 	delete app;

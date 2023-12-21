e44cb64ebe64d948122f05d5f0c4c0cc6968e38d
ryansrampage <ryanslee.47@gmail.com>
2023-12-21 15:25:36 +0800

    Added Premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "BumbuEngine"
+    architecture "x64"
+    
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}" 
+
+project "BumbuEngine"
+    location "BumbuEngine"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "BE_PLATFORM_WINDOWS",
+            "BE_BUILD_DLL",
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "BE_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "BE_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "BE_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "BumbuEngine/vendor/spdlog/include",
+        "BumbuEngine/src"
+    }
+
+    links
+    {
+        "BumbuEngine"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "BE_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "BE_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "BE_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "BE_DIST"
+        optimize "On"
